64a629cc3f7bc95001cd0a33ac5c96b3f888a2c1
Rishab Maharjan <gunnerrishav@gmail.com>
2020-02-12 10:55:15 +0545

    Initial Commit

---
+++ collapsingToolbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collapsingToolbar
+//
+//  Created by Rishab Maharjan on 2/12/20.
+//  Copyright © 2020 Rishab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
